dd28e32e9b5b47be0ece25edb0d61ca70ed38b49
Ilker Sezer <drilkersezer@hotmail.com>
2019-12-09 19:58:14 +0300

    Update

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -4,14 +4,21 @@ import { StoreModule } from '@ngrx/store';
 import * as fromTest from './reducers/test.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { TestEffects } from './effects/test.effects';
+import { TestTreeComponent } from './test-tree/test-tree.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TestTreeComponent],
   imports: [
     CommonModule,
     StoreModule.forFeature(fromTest.testFeatureKey, fromTest.reducer),
-    EffectsModule.forFeature([TestEffects])
+    EffectsModule.forFeature([TestEffects]),
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class TestModule {
--- src/app/views/pages/company/company.module.ts
+++ src/app/views/pages/company/company.module.ts
@@ -7,19 +7,28 @@ import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {CompanyResolver} from '../../../core/company/resolvers/company.resolver';
 import {EffectsModule} from '@ngrx/effects';
+import { CompanyTreeComponent } from './company-tree/company-tree.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material';
 
 const routes: Routes = [
   { path: '', component: CompanyMainComponent, resolve: { company: CompanyResolver}}
 ];
 
 @NgModule({
-  declarations: [CompanyMainComponent],
+  declarations: [CompanyMainComponent, CompanyTreeComponent],
   imports: [
     CommonModule,
     HttpClientModule,
     RouterModule.forChild(routes),
     StoreModule.forFeature(companyFeatureKey, companyReducer),
-    EffectsModule.forFeature([CompanyEffects])
+    EffectsModule.forFeature([CompanyEffects]),
+    MatCardModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [CompanyResolver]
 })
